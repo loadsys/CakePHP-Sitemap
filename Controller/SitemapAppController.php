1c4c5990d2ffde0dfb2b42c79b35227a1a77aef4
Justin Yost <justin.yost@yostivanich.com>
2014-11-09 23:24:40 -0800

    Improved Documentation in the SitemapAppController
    References: #13
    Signed-off-by: Justin Yost <justin.yost@yostivanich.com>

--- Controller/SitemapAppController.php
+++ Controller/SitemapAppController.php
@@ -1,7 +1,12 @@
 <?php
-
+/**
+ * SitemapAppController
+ */
+App::uses('Controller', 'Controller');
 App::uses('AppController', 'Controller');
 
+/**
+ * SitemapAppController
+ */
 class SitemapAppController extends AppController {
-
 }
